c3c3e784d551589c69f1a6d94dd7f59906a561d5
Diego <dhsachicaa@gmail.com>
2022-06-16 19:44:28 -0500

    Implementacion clase UbicacionGeografica

---
+++ app/src/main/java/Mundo/mundo.kt
@@ -0,0 +1,16 @@
+package Mundo
+
+data class UbicacionGeografica (var calle: Int, var carrera: Int) : Comparable<UbicacionGeografica> {
+    override fun compareTo(other: UbicacionGeografica): Int {
+        if (this.calle > other.calle)
+            return 1
+        else if (other.calle > this.calle)
+            return -1
+        else if (this.carrera > other.carrera)
+            return 1
+        else if (other.carrera > other.calle)
+            return -1
+        else
+            return 0
+    }
+}
