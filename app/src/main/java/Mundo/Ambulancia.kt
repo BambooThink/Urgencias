d0875499b62e13e4d5f5753b6827130281a52ccc
Diego <dhsachicaa@gmail.com>
2022-06-16 20:50:30 -0500

    Implementacion clase Ambulancia

--- app/src/main/java/Mundo/Ambulancia.kt
+++ app/src/main/java/Mundo/Ambulancia.kt
@@ -1,9 +1,23 @@
 package Mundo
 
-data class Ambulancia(var codigo: Int) {
+data class Ambulancia(var accidentado: Accidentado?, var estado: String, var ubicacion: UbicacionGeografica) {
 
     fun ingresar_accidentado(accidentado: Accidentado) {
+        if (this.estado.equals("VACIA")) {
+            this.estado = "OCUPADA"
+            this.accidentado = accidentado
+        }
+    }
 
+    fun desocupar() {
+        if (this.estado.equals("OCUPADA")){
+            this.accidentado = null
+            this.estado = "VACIA"
+        }
+    }
+
+    fun cambiar_ubicacion(nueva_ubicacion: UbicacionGeografica) {
+        this.ubicacion = nueva_ubicacion
     }
 
 }
