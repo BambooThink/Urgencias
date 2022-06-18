51cc82545c0121dc9404c5347ad7faf7a7c278e1
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-06-17 20:51:18 -0500

    Creación vistas

---
+++ app/src/main/java/com/mv/urgencias/IngresarNuevaAmbulancia.kt
@@ -0,0 +1,11 @@
+package com.mv.urgencias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngresarNuevaAmbulancia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_info_ambulancia)
+    }
+}
---
+++ app/src/main/java/com/mv/urgencias/IngresarNuevoHospital.kt
@@ -0,0 +1,11 @@
+package com.mv.urgencias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngresarNuevoHospital : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_nuevo_hospital)
+    }
+}
