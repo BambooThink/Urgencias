815bcadff516d84bac92bfaa4520ef1ca745dd8f
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-06-17 19:06:19 -0500

    Creación clase comparable Hospital

--- app/src/main/java/Mundo/Hospital.kt
+++ app/src/main/java/Mundo/Hospital.kt
@@ -1,11 +1,43 @@
 package Mundo
 
-abstract class Hospital(private var codigo: String) : Comparable<Hospital> {
+data class Hospital (var codigo: String) : Comparable<Hospital> {
+
+    private var pacientes: MutableList<String> = mutableListOf()
+    private var accidentes: MutableList<String> = mutableListOf()
+
+    fun getPacientes() = pacientes
+
+    fun getAccidentes() = accidentes
+
+    fun addPaciente(nombrePaciente: String) {
+        pacientes.add(nombrePaciente)
+    }
+
+    fun addAccidente(nombreAccidente: String) {
+        pacientes.add(nombreAccidente)
+    }
 
     override fun compareTo(other: Hospital): Int {
-        if (this.codigo == other.codigo)
-            return 1
-        else
-            return 0
+        if (this.codigo == other.codigo) return 1
+        else return 0
     }
+
+    fun consultarPaciente(nombrePaciente: String): Boolean {
+        return pacientes.find { it == nombrePaciente } != null
+    }
+
+    fun consultarAccidente(nombreAccidente: String): Boolean {
+        return accidentes.find { it == nombreAccidente } != null
+    }
+
+    fun ingresarAccidentado(nombreAccidentado: String, nombreAccidente: String) {
+        if (!consultarPaciente(nombreAccidentado) && consultarAccidente(nombreAccidente)) {
+            addPaciente(nombreAccidentado)
+        }
+    }
+
+    fun darAltaPaciente(nombrePaciente: String) {
+        pacientes.remove(pacientes.find { it == nombrePaciente})
+    }
+
 }
--- app/src/main/java/Mundo/calcularEsquemaManhattan.kt
+++ app/src/main/java/Mundo/calcularEsquemaManhattan.kt
@@ -3,5 +3,5 @@ package Mundo
 import kotlin.math.absoluteValue
 
 fun calcularEsquemaManhattan(x1: Int, y1: Int, x2: Int, y2: Int): Int {
-    return (x1 + x2).absoluteValue + (y1 + y2).absoluteValue
+    return (x1 - x2).absoluteValue + (y1 - y2).absoluteValue
 }
