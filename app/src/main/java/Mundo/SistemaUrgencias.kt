45e68a56a030b81944d6ac3fe913624e3ee6c127
Diego <dhsachicaa@gmail.com>
2022-06-17 19:28:20 -0500

    Actualizacion clase Hospital

--- app/src/main/java/Mundo/SistemaUrgencias.kt
+++ app/src/main/java/Mundo/SistemaUrgencias.kt
@@ -10,17 +10,17 @@ class SistemaUrgencias {
         var lista_ambulancias: TArrayList<Ambulancia>()
         var lista_hospitales: TArrayList<Hospital>()
 
-        fun agregar_ambulancia(var codigo: Int, var calle: Int, var carrera: Int) {
+        fun agregar_ambulancia(codigo: Int, calle: Int, carrera: Int) {
             if (lista_ambulancias.filter { it.codigo.equals(codigo) }.isEmpty)
-                lista_ambulancias.add(Ambulancia(null, codigo, "LIBRE", UbicacionGeografica(calle, carrera)))
+                lista_ambulancias.add(Ambulancia(codigo, null, "LIBRE", UbicacionGeografica(calle, carrera)))
         }
 
-        fun agregar_hospital(var codigo: Int, var nombre: String, var calle: Int, var carrera: String, var accidente1: String, var accidente2: String) {
+        fun agregar_hospital(codigo: Int, nombre: String, calle: Int, carrera: String, accidente1: String, accidente2: String) {
             if (lista_hospitales.filter { it.codigo.equals(codigo) }.isEmpty)
-                lista_hospitales.add(Hospital(codigo))
+                lista_hospitales.add(Hospital(codigo, ))
         }
 
-        fun ocurrio_accidente(var accidentado: Accidentado, var calle: Int, var carrera: Int) {
+        fun ocurrio_accidente(accidentado: Accidentado, calle: Int, carrera: Int) {
             lista_ambulancias.filter { it.estado.equals("LIBRE") }
                 .map { calcularEsquemaManhattan(it.ubicacion.calle, calle, it.ubicacion.carrera, carrera) }
                 .min()
